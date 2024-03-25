3be68b6835883b71a9bbc8e93e9f5f0417be2c83
nnbaitei <119753018+nnbaitei@users.noreply.github.com>
2024-03-25 11:28:15 +0700

    Update image_subscriber.py

--- my_cam/src/image_subscriber.py
+++ my_cam/src/image_subscriber.py
@@ -8,36 +8,28 @@ from sensor_msgs.msg import Image, LaserScan
 import random
 
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-# global angle 
 
 def callback_lidar(msg):
-    # laser_data = msg.ranges[600]
-    # print(laser_data)
-    # global angle
-    k = 0
     p = 600 #center point
     if angle != "Nan":
-        min = int(angle[0]) + p + k
-        max = int(angle[1]) + p + k
+        #msg.ranges[600] = 0 degree
+        min = int(angle[0]) + p 
+        max = int(angle[1]) + p 
         lst = msg.ranges[min:max]
         v = 10
+        #different robot and lidar is 8cm
         for i in lst:
             curr = i
             if v > curr:
                v = curr - 0.08
         print(f"{angle[2]} : {v} {angle[0], angle[1]}")        
-    # if angle == 'Nan':
-    #     # print("No objects higher than 25")
-    #     print('')
-    # else:
-    #     # print("no angle")
-    #     print('')
-    
 
 def callback_img(image_msg):
     try:
         cv_image = bridge.imgmsg_to_cv2(image_msg)
-        cv_image_copy = cv_image.copy()
+        #copy image
+        cv_image_copy = cv_image.copy()      
+        #flip image
         cv_image_copy = cv2.flip(cv_image_copy, 0)
         cv_image_copy = cv2.flip(cv_image_copy, 1)
         height = cv_image_copy.shape[0] #480
@@ -47,10 +39,12 @@ def callback_img(image_msg):
         degree_w = 53.5
         degree_h = 41.41
         degree_px = 53.5/width
+        #use yolo to detect object return x1, y1, x2, y2, label
         tensor_frame = model(cv_image_copy, size=640)
         data_frame = tensor_frame.pandas().xyxy[0]
         indexes = data_frame.index
         for index in indexes:
+            #find distance in horizontal axis from center and find degree of of x1, x2
             x1 = int(data_frame['xmin'][index])
             y1 = int(data_frame['ymin'][index])
             dist_center_obj_min = center_w - x1
@@ -60,19 +54,13 @@ def callback_img(image_msg):
             dist_center_obj_max = center_w - x2
             degree_center_obj_max = degree_px*dist_center_obj_max
             label = data_frame['name'][index]
-            # cv2.rectangle(cv_image_copy, (x1,y1), (x2,y2), (255,255,0), 1)
             global angle
+            #find object that first height less than 25 -> lidar can scan and find
             if y2 - y1 > 150 and y1 < 25:
                     angle = [degree_center_obj_max, degree_center_obj_min, label]
-                    #    calc(angle)
-                    #print(laser_data)
-                    #lidar_subcriber = rospy.Subscriber('/scan', LaserScan, lidar)
-                    #angle_pub = rospy.Publisher("/send_angle", 20)
-                    #dist_sub = rospy.Subscriber("/send_dist")
                     label = data_frame['name'][index]
                     conf = data_frame['confidence'][index]
                     text = label + ' (' + str(round(degree_center_obj_min, 2)) + ' ' + str(round(degree_center_obj_max, 2)) + ')'
-                    # text = label+ 'x1:' + str(x1) + 'y1:' + str(y1) + 'x2:' + str(x2) +'y2:' + str(y2)
                     cv2.rectangle(cv_image_copy, (x1,y1), (x2,y2), (255,255,0), 1)
                     cv2.putText(cv_image_copy, 
                                 text, 
